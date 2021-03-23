841f24e0cab7183256b4c7c39587f88935970dc7
NotPerry <48336059+NotPerry@users.noreply.github.com>
2021-03-23 10:16:51 -0400

    Update hash.py
    Adding beef

--- hash.py
+++ hash.py
@@ -1,1 +1,28 @@
+#os.listdir
+#os.stat
+#os.walk
+import hashlib
 
+#Run through filesystem
+def flier():
+
+#Use SHA2 (SHA256) so that each file is hashed as it moves through the file system
+def hasher():
+  
+
+#Need:
+#   Filename with full path
+#   Hash
+#   date/time file was observed
+
+#run and update the hash information, upon completion it should print out summary information that includes 
+#   all new files found, 
+#   any missing files, and 
+#   any file that was modified.
+
+#Extra Credit
+#Goal: Detect that a file was moved
+#Add to your summary section an output that documents 
+#   where the file is now, and 
+#   where it was, and 
+#   the time of the last scan that saw it in the older location.
