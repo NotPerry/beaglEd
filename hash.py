3854d7314be710ba3c54fe757084ac01f1348f40
NotPerry <amperry2017@gmail.com>
2021-03-30 21:11:29 -0700

    moving test.py into hash.py

--- hash.py
+++ hash.py
@@ -1,91 +1,68 @@
-#os.listdir
-#os.stat
-#os.walk
-from dirhash import dirhash
-import time
-import hashlib
 import os
+import hashlib
+from dirhash import dirhash
+import csv
+import time #All time formats are MMDDYYYY_hhmmss
 
-#Run through filesystem
-def flier(forDir):
-    hashEd = hasher(forDir)
-    #time = get time funciton
-    return #filename, hashEd, time
+#Global Variables
+string = ""
+string2 = ""
+r2 = []
+d2 = []
+ignore_list = ["/dev", "/proc", "/run", "/sys", "/tmp", "/var/lib", "/var/run"]
 
-#Use SHA2 (SHA256) so that each file is hashed as it moves through the file system
-def hasher(path):
-    dir_sha256 = dirhash(path, "sha256")
-    return dir_sha256
+#os.walk goes through and gets all of the information from the current directory
+for root, dirs, files in os.walk(os.getcwd()):
+    
+    #This if condition removes any directories and files within the forbidden root list
+    if root in ignore_list:
+        dirs[:] = []
+        files[:] = []
 
-#Need:
-#   Filename with full path
-#   Hash
-#   date/time file was observed
+    #This loops through all the files, hashes them, and stores them into array R2
+    #R2 will be written into the CSV file later in the process
+    for name in files:
+        try:
+            f = open(os.path.join(root, name))
+            bob = f.read()
+            f.close()
+            m = hashlib.sha256()
+            m.update(bob.encode())
+            hshed = m.hexdigest()
+            f1 = {}
+            f1['path'] = os.path.join(root, name)
+            f1['hash'] = hshed
+            f1['access'] = time.strftime("%m%d%Y_%H%M%S")
+            r2.append(f1)
+        except:
+            string +=  os.path.join(root, name) + "\n"
+    
+    #This loops through all the directories, hashes them, then outputs them to d2 
+    #D2 will be written to the CSV
+    for name in dirs:
+        try:
+            dir_sha256 = dirhash(os.path.join(root, name), "sha256")
+            d1 = {}
+            d1['path'] = os.path.join(root, name)
+            d1['hash'] = dir_sha256
+            d1['access'] = time.strftime("%m%d%Y_%H%M%S")
+            d2.append(d1)
+        except:
+            string2 += root+name+"\n"
 
+#This writes the output of our hashes to the CSV file
+with open('FieSys_' + time.strftime("%m%d%Y_%H%M%S") + '.csv', 'w') as joe:
+    fieldname = ['path', 'hash', 'access']
+    writer = csv.DictWriter(joe, fieldnames=fieldname)
+    writer.writeheader()
+    for data in r2:
+        writer.writerow(data)
+    for data in d2:
+        writer.writerow(data)
 
-
-def main():
-    #call flier
-    #write contents to file
-    string = ""
-    for root, dirs, files in os.walk("."):
-        for name in root:
-            try:
-                dir_sha256 = dirhash(root, "sha256")
-                string += root + "\t\t\t" + dir_sha265 + "\n"
-            except:
-                string += root+name+"\n"
-        for name in files:
-            try:
-                f = open(os.path.join(root, name), "r")
-                contents = f.read()
-                f.close()
-                m = hashlib.sha256()
-                m.update(contents)
-                hsh = m.hexdigest()
-                string += os.path.join(root, name) + "\t\t\t" + hsh + "\n"
-            except:
-                string += root + name + "\n"
-    print(string)
-
-#run and update the hash information, upon completion it should print out summary information that includes 
-#   all new files found, 
-#   any missing files, and 
-#   any file that was modified.
-
-def compare():
-    '''
-    import glob
-    import os
-    file = glob.glob("*.ipynb")
-    file.sort(key=os.path.getmtime)
-    This will list the most recent file, we could use this to find our most recent baseline to compare our data to
-    old_fd.lseek(0)
-    new_fd.lseek(0)
-'''
-    for x in range(0, len(new_fd)):
-        buff = []
-        if new_fd[x] not in old_fd:
-            #color green
-            print("Change detected {}: {} {}".format(buff[2], buff[0], buff[1]))
-
-    for x in range(0, len(old_fd)):
-        buff = []
-        buff = old_fd[x].split(",")
-
-        if buff[1] not in new_fd:
-        #color red
-            print("Change detected {}: {} {} ".format(get_time, buff[0], buff[1]))
-
-
-
-#Extra Credit
-#Goal: Detect that a file was moved
-#Add to your summary section an output that documents 
-#   where the file is now, and 
-#   where it was, and 
-#   the time of the last scan that saw it in the older location.
-
-
-if __name__ == "__main__":
-    main()
+#Despite our best efforts, some files are not able to be read due to unicode errors and the like
+#We have added these files to this list in order for administrators to track them more closely
+f = open("errorFiles"+time.strftime("%m%d%Y_%H%M%S") , 'w')
+f.write("The following files were not able to be read:\n" + string)
+f.write("\n\nThe following directories were not able to be read:\n" + string2)
+f.close()
