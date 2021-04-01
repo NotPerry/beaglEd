454a65f4b85bbe926206b0c53016b3c13cd61917
NotPerry <amperry2017@gmail.com>
2021-04-01 08:35:40 -0700

    final Changes

--- hash.py
+++ hash.py
@@ -3,6 +3,8 @@ import hashlib
 from dirhash import dirhash
 import csv
 import time #All time formats are MMDDYYYY_hhmmss
+import sys
+import getopt
 
 #Global Variables
 string = ""
@@ -10,9 +12,31 @@ string2 = ""
 r2 = []
 d2 = []
 ignore_list = ["/dev", "/proc", "/run", "/sys", "/tmp", "/var/lib", "/var/run"]
+compare = True
+
+opts, args = getopt.getopt(sys.argv[1:], "ho")
+
+def usage():
+    print("You need to include a comparison CSV file.")
+    print("Usage: python hash.py <comparison.csv>")
+    print("For initial run, go: python hash.py -o")
+    sys.exit() 
+
+if (len(sys.argv) == 1):
+    usage()   
+elif (len(sys.argv) == 2) and (sys.argv[-1][-4:] == ".csv"):
+    compFile = sys.argv[-1]
+elif (len(sys.argv) == 2):
+    for o, a in opts: 
+        if o == "-o":
+            compare = False
+        else:
+            usage()
+else:
+    usage()
 
 #os.walk goes through and gets all of the information from the current directory
-for root, dirs, files in os.walk(os.getcwd()):
+for root, dirs, files in os.walk("/"):
     
     #This if condition removes any directories and files within the forbidden root list
     if root in ignore_list:
@@ -51,7 +75,8 @@ for root, dirs, files in os.walk(os.getcwd()):
             string2 += root+name+"\n"
 
 #This writes the output of our hashes to the CSV file
-with open('FieSys_' + time.strftime("%m%d%Y_%H%M%S") + '.csv', 'w') as joe:
+newFile = str('FieSys_' + time.strftime("%m%d%Y_%H%M%S") + '.csv')
+with open(newFile, 'w') as joe:
     fieldname = ['path', 'hash', 'access']
     writer = csv.DictWriter(joe, fieldnames=fieldname)
     writer.writeheader()
@@ -60,9 +85,52 @@ with open('FieSys_' + time.strftime("%m%d%Y_%H%M%S") + '.csv', 'w') as joe:
     for data in d2:
         writer.writerow(data)
 
-#Despite our best efforts, some files are not able to be read due to unicode errors and the like
+#Despite our best efforts, some files are not able to be read due to unicode errors and not able to run $sudo python ...
 #We have added these files to this list in order for administrators to track them more closely
 f = open("errorFiles"+time.strftime("%m%d%Y_%H%M%S") , 'w')
 f.write("The following files were not able to be read:\n" + string)
 f.write("\n\nThe following directories were not able to be read:\n" + string2)
 f.close()
+
+#This section defines the functions to compare the new file to the comparison (original) file.
+class bcol:
+    yel = '\033[93m'
+    blu = '\033[96m'
+    red = '\033[91m'
+    noc = '\033[0m'
+    bold = '\033[1m'
+
+def check_mod(dir_path, fileone):
+    for line in fileone:
+        buff = []
+        buff = line.split(",")
+        if buff[0] == dir_path:
+            return True
+    return False
+
+def compare_csv(original, compare):
+    dir_list = []
+    with open(original, 'r') as t1, open(compare, 'r') as t2:
+        fileone = t1.readlines()
+        filetwo = t2.readlines()
+
+    for line in filetwo:
+        buff = []
+        buff = line.split(",")
+        dir_list.append(buff[0])
+        if line not in fileone:
+            flag = check_mod(buff[0], fileone)
+            if (flag == True): #if it detected there was a modification
+                print("{}{}{} modified. Observed at: {}{}{}".format(bcol.yel, buff[0], bcol.noc, bcol.bold, buff[2], bcol.noc))
+            if (flag == False): #if it detected there was a new file
+                print("{}{}{} added. Observed at: {}{}{}".format(bcol.blu, buff[0], bcol.noc, bcol.bold, buff[2], bcol.noc))
+    for line in fileone:
+        buff = []
+        buff = line.split(",")
+        if buff[0] not in dir_list:
+            print("{}{}{} removed. Observed at: {}{}{}".format(bcol.red, buff[0], bcol.noc, bcol.bold, time.strftime("%m%d%Y_%H%M%S"), bcol.noc))
+
+#Ensures there is a file to compare, and this isn't the original copy
+if compare == True:
+    compare_csv(newFile, compFile)
+
